6ab03ba04b87dc8242c9f046fbe83cdd0e283abc
Martin Zamarski <34303217+momom88@users.noreply.github.com>
2018-02-22 17:31:20 +0100

    Add files via upload

--- MusicPlayer/app/src/main/java/com/example/android/musicplayer/MusicPlayer.java
+++ MusicPlayer/app/src/main/java/com/example/android/musicplayer/MusicPlayer.java
@@ -9,7 +9,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MusicPlayer extends AppCompatActivity {
+public class MusicPlayer extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,62 +29,47 @@ public class MusicPlayer extends AppCompatActivity {
         final Bundle extras = getIntent().getExtras();
 
         // Getting the Views from xml
-
         songTextView.setText(extras.getString("authorName"));
         authorTextView.setText(extras.getString("nameSong"));
         Image.setImageResource(extras.getInt("image"));
 
         // Find the ImageView in the MusicPlayer.xml layout with the ID image.
         ImageView stopImage = (ImageView) findViewById(R.id.stop);
-
+        stopImage.setOnClickListener(this);
         // Find the ImageView in the MusicPlayer.xml layout with the ID image.
         ImageView playImage = (ImageView) findViewById(R.id.play);
-
+        playImage.setOnClickListener(this);
         // Find the ImageView in the MusicPlayer.xml layout with the ID image.
         ImageView pauseImage = (ImageView) findViewById(R.id.pause);
-
+        pauseImage.setOnClickListener(this);
         // Find the ImageView in the MusicPlayer.xml layout with the ID image.
         ImageView backImage = (ImageView) findViewById(R.id.back);
+        backImage.setOnClickListener(this);
+    }
 
-        //after clicking on the image Stop, Toust writes a message: Stop
-
-        stopImage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+    //implement the onClick method here for image : stop,play,pause and back
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.stop:
                 Toast.makeText(MusicPlayer.this,
                         "Stop",
                         Toast.LENGTH_LONG).show();
-            }
-        });
-
-        // after clicking on the image Play, Toust writes a message: Play
-        playImage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+                break;
+            case R.id.play:
                 Toast.makeText(MusicPlayer.this,
                         "Play",
                         Toast.LENGTH_LONG).show();
-            }
-        });
-
-        // after clicking on the image pause, Toust writes a message: Pause
-        pauseImage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+                break;
+            case R.id.pause:
                 Toast.makeText(MusicPlayer.this,
                         "Pause",
                         Toast.LENGTH_LONG).show();
-            }
-        });
-
-        // after clicking on the image back, Start activity TracklistActivity
-        backImage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MusicPlayer.this, TracklistActivity.class);
+                break;
+            case R.id.back:
+                Intent intent = new Intent(this, TracklistActivity.class);
                 startActivity(intent);
-            }
-        });
+                break;
 
+        }
     }
 }
